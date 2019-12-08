1f20ce01995aa14e4ca2fa1b3fda939151fdbc4a
Misa <Misa@DESKTOP-N0JSLO9>
2019-12-08 22:09:45 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Узел1");
+			System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Узел0", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+			this.button1 = new System.Windows.Forms.Button();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(557, 30);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(171, 83);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "ADD Group";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(53, 144);
+			this.treeView1.Name = "treeView1";
+			treeNode1.Name = "Узел1";
+			treeNode1.Text = "Узел1";
+			treeNode2.Name = "Узел0";
+			treeNode2.Text = "Узел0";
+			this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+			this.treeView1.Size = new System.Drawing.Size(942, 560);
+			this.treeView1.TabIndex = 2;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(128, 56);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(373, 31);
+			this.textBox1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1432, 919);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.treeView1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	// вью
+	interface IView
+	{
+		void SetText(string str);
+		void SetTree(List<GroupServer> listserver);
+	}
+	public partial class Form1 : Form, IView
+	{
+		private Presenter presenter;
+		public Form1()
+		{
+			InitializeComponent();
+			presenter = new Presenter(this, new Model());
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			presenter.Load();
+		}
+		void IView.SetText(string str)
+		{
+			textBox1.Text = str;
+		}
+		void IView.SetTree(List<GroupServer> listserver)
+		{
+			for (int i = 0; i < listserver.Count; i++)
+			{
+				TreeNode newTree = new TreeNode();
+				newTree.Name = listserver[i].NameServer + "_item";
+				newTree.Text = listserver[i].NameServer;
+				newTree.ToolTipText = listserver[i].DescriptionServer;
+				treeView1.Nodes.Add(newTree);
+			}
+		}
+
+	}
+
+	// презентер
+
+	class Presenter
+	{
+		private readonly IView iview;
+		private readonly IModel imodel;
+
+		public Presenter(IView iview, IModel imodel)
+		{
+			this.iview = iview;
+			this.imodel = imodel;
+		}
+		public void Load()
+		{
+			string text = imodel.Load();
+			List<GroupServer> listserver = imodel.LoadTree();
+			iview.SetText(text);
+			iview.SetTree(listserver);
+
+		}
+	}
+
+	// модел
+
+	interface IModel
+	{
+		string Load();
+		List<GroupServer> LoadTree();
+	}
+	class GroupServer
+	{
+		public string NameServer { get; set; }
+		public string DescriptionServer { get; set; }
+		public GroupServer(string nameserver, string descserver)
+		{
+			NameServer = nameserver;
+			DescriptionServer = descserver;
+		}
+	}
+	class Model : IModel
+	{
+		string IModel.Load()
+		{
+			return "Text loaded";
+		}
+		List<GroupServer> IModel.LoadTree()
+		{
+			List<GroupServer> listserver = new List<GroupServer>();
+			listserver.Add(new GroupServer("0001", "Server 0001"));
+			listserver.Add(new GroupServer("0002", "Server 0002"));
+			return (listserver);
+		}
+	}
+
+
+}
+
